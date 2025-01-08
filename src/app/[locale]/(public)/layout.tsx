d991ad90778280c38c788bfb99205177ca40aa2e
Tahiry <tahirymanitra@gmail.com>
2025-01-08 21:05:39 +0300

    insert logo

--- src/app/[locale]/(public)/layout.tsx
+++ src/app/[locale]/(public)/layout.tsx
@@ -4,6 +4,7 @@ import { Label } from "@/app/_components/ui/label";
 import { getScopedI18n } from "@/packages/locales/server";
 import { Circle } from "lucide-react";
 import Link from "next/link";
+import Image from "next/image";
 
 const PublicLayout = async ({ children }: { children: React.ReactNode }) => {
   const t = await getScopedI18n("common");
@@ -13,12 +14,10 @@ const PublicLayout = async ({ children }: { children: React.ReactNode }) => {
       <header className="bg-white rounded-lg shadow-sm sticky py-4 top-0 z-10 px-4">
         <div className=" h-fit flex items-center justify-between">
           <Link href="/" className="flex items-center gap-2">
-            <Circle className="h-8 w-8 text-muted" />
-            <Label className="text-2xl font-bold text-muted cursor-pointer">
-              STAF
-            </Label>
+            <Image width={50} height={50} className="rounded-full" src="/LOGO-FPVM.webp" alt="Logo Staf" />
           </Link>
           <LoginFormModal />
+      
         </div>
       </header>
       {children}
