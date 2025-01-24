24fde68241b25c67cfab714ec9f39a0cf31e2af0
Akihiro <akihiro.takeda.dev@tchi.xyz>
2025-01-24 11:12:41 +0300

    feat: Implement getBranchByNameAction

--- src/services/branch/branch.action.ts
+++ src/services/branch/branch.action.ts
@@ -1,8 +1,12 @@
 'use server';
 
-import { createBranch } from "./branch.repository";
+import { createBranch, getBranchByName } from "./branch.repository";
 import { BranchInput } from "./branch.type";
 
 export const createNewBranch = async (input: BranchInput) => {
     await createBranch(input);
+}
+
+export const getBranchByNameAction = async (name: string) => {
+    return await getBranchByName(name);
 }
